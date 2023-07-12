7f059cdbebe3d98ee2540e27080c82c8c4b63a17
sathiya-2002 <132586388+sathiya-2002@users.noreply.github.com>
2023-07-12 13:04:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+       img.src="./images/on.img.png";
+}
+
+function bulb_off(){ 
+       img.src="./images/off.img.png";
+}
